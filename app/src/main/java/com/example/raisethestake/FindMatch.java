a21756ce59444e11c9959d615be016418b409889
sherrinfordh23 <thanhantrinh2015@gmail.com>
2022-11-15 13:24:42 -0500

    Added all empty activties

---
+++ app/src/main/java/com/example/raisethestake/FindMatch.java
@@ -0,0 +1,14 @@
+package com.example.raisethestake;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindMatch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_match);
+    }
+}
---
+++ app/src/main/java/com/example/raisethestake/SearchForPlayer.java
@@ -0,0 +1,14 @@
+package com.example.raisethestake;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchForPlayer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_for_player);
+    }
+}
